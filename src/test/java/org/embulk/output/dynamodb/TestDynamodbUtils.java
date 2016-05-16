a456f79d029d5f074ffa8421700be03b9a848c00
sakama <satoshiakama@gmail.com>
2016-05-16 13:44:01 +0900

    Remove unnecessary import

--- src/test/java/org/embulk/output/dynamodb/TestDynamodbUtils.java
+++ src/test/java/org/embulk/output/dynamodb/TestDynamodbUtils.java
@@ -1,10 +1,5 @@
 package org.embulk.output.dynamodb;
 
-import org.embulk.spi.TestPageBuilderReader.MockPageOutput;
-import org.junit.BeforeClass;
-
-import static org.junit.Assume.assumeNotNull;
-
 public class TestDynamodbUtils
 {
 }
